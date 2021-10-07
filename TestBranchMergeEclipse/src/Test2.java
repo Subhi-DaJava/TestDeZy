130f335efe006c3564be53e33f09b541244ea0c9
Subhi-DaJava <subhyfr809@gmail.com>
2021-10-07 21:56:31 +0200

    hot fix de Subhi

--- TestBranchMergeEclipse/src/Test2.java
+++ TestBranchMergeEclipse/src/Test2.java
@@ -2,6 +2,7 @@
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Tester les branches et pushu avec Eclipse.");
+		System.out.println("Hot fix est arrivé pour debugger le bug.");
 		
 	}
 
